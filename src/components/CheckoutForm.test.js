be344bea7bd14c10676a1decd999dc30c6df3909
Anhtuan Tran <anhtuantran.github@gmail.com>
2021-02-12 13:45:16 -0600

    MVP

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,35 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
-
+import userEvent from '@testing-library/user-event'
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />)
 
-test("form shows success message on submit with form details", () => {});
+    const header = screen.queryByText(/checkout form/i)
+    expect(header).toBeInTheDocument()
+
+});
+
+test("form shows success message on submit with form details", async () => {
+    render(<CheckoutForm />)
+    const firstNameInput = screen.getByLabelText(/first name/i)
+    const lastNameInput = screen.getByLabelText(/last name/i)
+    const addressInput = screen.getByLabelText(/address/i)
+    const cityInput = screen.getByLabelText(/city/i)
+    const stateInput = screen.getByLabelText(/state/i)
+    const zipInput = screen.getByLabelText(/zip/i)
+    const button = screen.getByRole('button');
+
+    userEvent.type(firstNameInput, "Anhtuan")
+    userEvent.type(lastNameInput, "Tran")
+    userEvent.type(addressInput, "101 Elm Street")
+    userEvent.type(cityInput, "Boston")
+    userEvent.type(stateInput, "Massachusetts")
+    userEvent.type(zipInput, "12345")
+    userEvent.click(button, {name: /checkout/i})
+
+    const successMessage = await screen.getByTestId('successMessage')
+    expect(successMessage).toBeInTheDocument();
+});
